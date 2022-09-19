d0938b3fe22b47c5c239783b8ba44e450d8e07b7
logeshwars <stylishstarlogesh15@gmail.com>
2022-09-19 23:11:00 +0530

    front end connected

--- app.js
+++ app.js
@@ -4,15 +4,17 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import path from "path";
 const app = express();
 const port = process.env.PORT || 3001;
+app.use(express.static("./build"));
 app.use(json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 dotenv.config();
 app.get("/", (req, res) => {
-  res.send("hello");
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use("/user", user);
