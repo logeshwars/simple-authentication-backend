6e5f8ff77a069e01001734b9ab0883c781f95844
logeshwars <stylishstarlogesh15@gmail.com>
2022-09-19 19:18:52 +0530

    express session removed

--- app.js
+++ app.js
@@ -3,24 +3,13 @@ import user from "./routers/user.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
-import session from "express-session";
 import bodyParser from "body-parser";
-import { randomUUID } from "crypto";
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
-app.use(
-  session({
-    genid: () => randomUUID(),
-    secret: process.env.SESSION_KEY,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true, expires: 60 * 60 * 24 },
-  })
-);
 dotenv.config();
 app.get("/", (req, res) => {
   res.send("hello");
