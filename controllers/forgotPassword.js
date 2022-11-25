830d40a6a6b4d1706020ea14e25139ded7410407
logeshwars <logeshwars15@gmail.com>
2022-11-25 16:06:42 +0530

    additional features added

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -3,17 +3,7 @@ import resConst from '../constants/responses.js';
 import responseCreator from '../utils/responseCreator.js';
 const prisma = new PrismaClient();
 const { status, messages } = resConst;
-const logout = async (req, res) => {
-	try {
-        
-		await prisma.logins.deleteMany({
-			where: { jwtid: jti },
-		});
-		res.clearCookie(jwtConst.AuthToken);
-		res.clearCookie(jwtConst.RefreshToken);
-		responseCreator(res, status.Accepted, messages.logout);
-	} catch (err) {
-		responseCreator(res, status.ServerError, messages.sww);
-	}
+const forgotPassword = async (req, res) => {
+	
 };
-export default logout;
+export default forgotPassword;
